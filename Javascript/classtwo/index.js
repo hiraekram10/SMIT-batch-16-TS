44fae3364a60e91f0596db29af11d805ff93490d
Umra-Badar20 <umrabadar555@gmail.com>
2025-07-26 20:07:48 +0500

    done with chapter 13

---
+++ Javascript/classtwo/index.js
@@ -0,0 +1,138 @@
+// Data type boolean
+
+// var jsLearn = false // true
+
+// syntax
+
+
+// if(condition ){
+// //      true statement
+// }
+
+// if (15 == "5"){
+//     console.log("match krraha");
+
+// }else{
+//     console.log("nhi mila");
+
+// }
+
+
+
+// if (5 == "5"){
+//     console.log("match krraha");
+
+// }else{
+//     console.log("nhi mila");
+
+// }
+
+
+// comparison operator
+
+//  ==  equality operator
+//  === strcit equality
+//  > greater than
+//  < lessthan
+//  <= less than or equal 
+//  >= greater than or equal 
+//  != not equal to
+//  !== strcit not equal
+
+
+// if(true){
+//     console.log("match");
+
+// }else{
+//    console.log(" unmatched")
+// }
+
+// if(""){
+//     console.log("match");
+
+// }else{
+//    console.log(" unmatched")
+// }
+
+// 0,-0
+// undefined
+// ""
+// false
+// Nan 
+// null
+
+
+// console.log(0 == false);
+// console.log("0" === false);
+// console.log("rat" == "Rat");
+
+
+
+// console.log(5 < 10); //true
+// console.log(8 < 7);  //false
+// console.log(11 >= 11 ); true
+// console.log(10 <= 3);  false
+
+
+// if( 5 != 6){
+//     console.log('yes')
+
+// }else{
+//     console.log("djhkfg");
+
+// }
+
+
+// console.log(5 !==  10);
+// console.log(false != 0);
+
+
+// else if
+// var num = prompt('Enter your age')
+
+// if (num >= 18) {
+//     console.log("you are allowed to watch movie");
+
+// }else if(num >= 15){
+//     console.log("you are allowed to play games");
+    
+// }else if (num >= 10) {
+//     console.log("you are allowed to watch cartoon");
+// }
+// else {
+//     console.log('read story');
+
+// }
+
+
+
+var userInp = +prompt()
+
+
+if (userInp % 2 == 0){
+    console.log("even");
+    
+}else{
+    document.write("<h1>odd</h1>" + userInp )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
