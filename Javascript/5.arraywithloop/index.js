b696aa078920e67fbfe0f644214c9c92a87bd5d9
Umra-Badar20 <umrabadar555@gmail.com>
2025-08-07 20:04:12 +0500

    done with break and loop

---
+++ Javascript/5.arraywithloop/index.js
@@ -0,0 +1,110 @@
+// for(var i = 10; i >= 1  ; i-- ){
+
+//     console.log(2 +  " x" + i + "=" + i*2 );
+
+
+// }
+// var names = ["hira","sana","warisha","iqra",[3,4 ,[true, false]]]
+// console.log(names[4][2][1]);
+
+
+// var names = ["hira","sana","warisha","dania"]
+
+
+// for(var i = 0 ; i < names.length ; i++){
+//     console.log(names[i]);
+
+// }
+
+
+
+
+
+
+// for(var i = 0 ; i < 4 ; i++){
+//     console.log(i);
+
+// }
+
+
+// console.log(i);
+
+
+// for (var i = 0; i < 4; i++) {
+//     console.log(i);
+
+//     if (i == 2) {
+//         break;
+//     }
+
+// }
+
+
+// for (var i = 0; i < 4; i++) {
+//     console.log(i);
+
+//     if (i == 2) {
+//         break;
+        
+        
+//     }
+//     console.log("hi");
+
+// }
+
+
+
+
+// for (var i = 0; i < 4; i++) {
+//   console.log(i);  //0
+  
+//         break;
+        
+        
+   
+
+// }
+
+
+
+// for (var i = 0; i < 4; i++) {
+
+  
+//    if(i > 2 ){
+//       console.log(i);
+//     break;
+// }
+        
+    
+   
+
+// }
+
+
+for (var i = 0; i < 4; i++) {
+
+  
+   if(" hi "){
+      console.log(i);
+    break;
+}
+
+console.log(i);
+
+    
+   
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
