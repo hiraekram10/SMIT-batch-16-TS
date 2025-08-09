1749630c3e3f70daabb7403e2e5dc5e04cf55b1f
Umra-Badar20 <umrabadar555@gmail.com>
2025-08-09 20:01:00 +0500

    done with loop break

---
+++ Javascript/6.Nestedloop/index.js
@@ -0,0 +1,113 @@
+// var userCity = prompt()
+
+// var arr = ['khi', "lhr", "isl", "pesh", "que"]
+
+// for (var i = 0; i < arr.length; i++) {
+
+//     if (userCity === arr[i]) {
+//         console.log("your city is the cleanest city");
+
+//     } else {
+//         console.log("safai krwao");
+
+//     }
+
+// }
+
+
+
+// var userCity = prompt()
+
+// var arr = ['khi', "lhr", "isl", "pesh", "que"]
+// var flag = false
+
+// for (var i = 0; i < arr.length; i++) {
+
+//     if (userCity === arr[i]) {
+//         console.log("your city is the cleanest city");
+//         flag = true
+
+//     } 
+
+// }
+
+// if(flag == false){
+//     console.log('your city is not in the list');
+
+// }
+
+
+
+// for (var i = 0; i < 4; i++) {
+
+//     for (var j = 0; j < 3; j++) {
+
+
+//         console.log(i, j);
+
+//     }
+
+
+//         break;
+
+// }
+
+
+// var nums = [[1, 3, 2], [6, 3, 1,5]]
+
+
+// for (var i = 0; i < nums.length; i++) {
+                     
+//     for (var j = 0; j < nums[i].length; j++) {
+
+//  console.log(nums[i][j] * 2); 
+             
+//     }
+
+// }
+
+// var intro = "hi my name is hira"
+// var copy = intro.slice(14)
+// console.log(copy);
+
+
+// var name= "RAZA"
+// name = name.toLowerCase()
+// console.log(name);
+
+
+// var abc = "hello world"
+// for(var i =0; i < abc.length;i++){
+//     console.log(abc[i]);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
