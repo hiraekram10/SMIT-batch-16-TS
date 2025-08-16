35de932983040d2308c61ab390507d12ad5058a4
Umra-Badar20 <umrabadar555@gmail.com>
2025-08-16 20:08:10 +0500

    start string method

---
+++ Javascript/7.stringMethods/index.js
@@ -0,0 +1,39 @@
+// var name = "sana"
+// var newName =name.slice(0,1)
+// var remaining = name.slice(1)
+// console.log(newName.toUpperCase() + remaining);
+
+var name = 'samiya khan'
+var result;
+var temp;
+
+
+for (var i = 0; i < name.length; i++) {
+
+    if (i === 0) {
+        result = name[i].toUpperCase()
+
+    }
+    else if (name[i] === ' ') {
+        temp = name[i + 1].toUpperCase()
+
+    }
+    else {
+        if (temp) {
+            result += temp
+            temp =''
+      
+        } else {
+            result += name[i]
+        }
+    }
+
+
+
+
+}
+
+console.log(result);
+
+
+
