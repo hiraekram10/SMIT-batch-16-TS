18282eed4eae29fe39a127cfcb9a6b8f5248cb8f
Umra-Badar20 <umrabadar555@gmail.com>
2025-08-30 20:07:37 +0500

    cuntion started ,para, arg scoping

---
+++ Javascript/10.Dates.Func/index.js
@@ -0,0 +1,114 @@
+// var d = new Date();
+// d.setMonth(3)
+// console.log(d);
+
+
+// console.log(moment()._d);
+
+// var formatedate = moment().format('l, hh')
+// console.log(formatedate);
+
+
+// function greeting (){  //function declaration
+//   console.log('hello');
+  
+// }
+
+// function invoke (call)
+// greeting() //hello
+// greeting()  //hello
+// greeting()  //hello
+// greeting()  //hello
+
+
+
+// function greeting (){  //function declaration
+// var date = moment()._d
+// console.log(date);
+
+  
+// }
+// greeting()
+// greeting()
+// greeting()
+// greeting()
+
+// 
+//parameters
+// function calculate (num1,num2,email){  
+// var add = num1 + num2
+
+// console.log(add,email);
+
+
+
+  
+// }
+// // aurg
+
+// calculate(3,4,"hera@gmail.com")
+// calculate(1000,44,"sana@gmail.com")
+
+
+// var email = document.getElementById('email')
+// var password = document.getElementById('pass')
+
+// function signIn(){
+//   if(email.value == 'hira@gmail.com' && password == '12345'){
+//     alert('sign in successfully')
+//   }else{
+//      alert('Error in auth')
+//   }
+    
+// }
+
+
+// var email = document.getElementById('email')
+// var password = document.getElementById('pass')
+// function signIn(){
+// var val =  email.value
+// if(val.includes('@')){
+//   console.log('valid');
+  
+// }else{
+//    email.style.backgroundColor = 'red'
+// }
+
+    
+// }
+
+
+var str = 'hasdh'
+function mssg(a,b){
+console.log(str);
+
+    var text ='hello'
+  return text
+ 
+  
+}
+
+console.log(mssg())
+// console.log(mssg())
+// console.log(mssg())
+// console.log(mssg())
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
