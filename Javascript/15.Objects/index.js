8f69a6949d31f93fec04c27a9447b4849bd937d0
Umra-Badar20 <umrabadar555@gmail.com>
2025-09-20 20:02:46 +0500

    object half

---
+++ Javascript/15.Objects/index.js
@@ -0,0 +1,146 @@
+// var inst = {
+//     name:"SMIT",
+//     locationn:'aliabad',
+//     PlotNo:346,
+    
+// }
+// // dot notation
+// // console.log(inst.name);
+// // console.log(inst.locationn);
+// // console.log(inst.PlotNo);
+
+
+// // bracket notation
+
+// // console.log(inst['locationn']);
+// console.log(inst['name']);
+
+
+// var inst = {
+//     name:"SMIT",
+//     locationn:'aliabad',
+//     PlotNo:346,
+//     accessories: ["computers","blubs","chairs"]
+    
+// }
+
+// for(var i =0; i < inst.accessories.length;i++){
+//     console.log(inst.accessories[i]);
+    
+// }
+
+// console.log(Object.keys(inst)); ['name','location']
+
+// copy by ref
+
+// var inst = {
+//     name:"SMIT",
+//     locationn:'aliabad',
+//     PlotNo:346,
+//     accessories: ["computers","blubs","chairs"]
+    
+// }
+// // spread operator
+
+// var newVar = {...inst}
+// updation value
+// inst.locationn ='numaish'
+// console.log(inst);
+// console.log(newVar);
+
+// var inst = {
+//     name:"SMIT",
+//     locationn:'aliabad',
+//     PlotNo:346,
+//     accessories: ["computers","blubs","chairs"]
+    
+// }
+
+// inst = "change"
+//  console.log(inst);
+
+
+// var student = {}
+
+
+// student.name ="hello"
+// console.log(student);
+
+// var student = {
+//     name:'talha',
+//     no:283746,
+//     email:'talha@gmail.com',
+//     personalInfo: {
+//         address: '76235 gulshan',
+//         city:"karachi",
+//         num:{
+//             val:923784
+//         }
+//     }
+// }
+
+
+
+// console.log(student.personalInfo.address);
+// console.log(student.rollNo.num); //Cannot read properties of undefined (reading 'num')
+//  console.log(student.personalInfo.opt.val);
+
+
+
+// var student = {
+//     name:'talha',
+//     no:283746,
+//     email:'talha@gmail.com',
+   
+     
+// }
+
+// student.personalInfo = {}
+// student.personalInfo.city='some'
+// console.log(student);
+
+
+// var student = {
+//     name:'talha',
+//     no:283746,
+//     email:'talha@gmail.com',
+   
+     
+// }
+// console.log('emai' in student); //true/false
+
+
+// var student = {
+//     name:'talha',
+//     no:283746,
+//     email:'talha@gmail.com',
+//    getName : function (){
+//        console.log(name);
+       
+//    }
+     
+// }
+
+// student.getName()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
