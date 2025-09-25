01a0596d3efef6c6ed6a15051b2f01ef5bec5594
Umra-Badar20 <umrabadar555@gmail.com>
2025-09-25 20:04:53 +0500

    constructor done

---
+++ Javascript/16.ObjContinue/index.js
@@ -0,0 +1,93 @@
+
+// var student = {
+//     fullname:'talha',
+//     no:283746,
+//     email:'talha@gmail.com',
+//    getName : function (){
+//      console.log(this.fullname+ "your email is" + this.email);
+     
+       
+       
+//    }
+     
+// }
+// var student2 = {
+//     fullname:'user',
+//     no:283746,
+//     email:'user@gmail.com',
+//    getName : function (){
+//      console.log(this.fullname+ "your email is" + this.email);
+     
+       
+       
+//    }
+     
+// }
+// student.getName()
+// student2.getName()
+
+// this
+// console.log(this);
+var main = document.querySelector('#main')
+// constructor function
+// function Student (n,e,p,i){
+//    this.name =n ;
+//    this.email = e;
+//    this.password =p
+//    this.image = i 
+// }
+// var st1 = new Student("hira", 'hira@gmail.com',1234,'two.png')
+// var st2 = new Student("sana", 'sana@gmail.com',1232344,'two.png')
+// var st3 = new Student("iqra", 'iqra@gmail.com',344,'two.png')
+// var st4 =new Student("bisma", 'bisma@gmail.com',2342,'two.png')
+// var students = [st1,st2,st3,st4]
+
+
+// for(var i =0; i <students.length;i++){
+//   main.innerHTML += `<li class="card" style="width: 18rem;">
+//   <img src="${students[i].image}" class="card-img-top" alt="...">
+//   <div class="card-body">
+//     <h5 class="card-title">${students[i].name}</h5>
+//     <p class="card-text">${students[i].email}</p>
+//     <a href="#" class="btn btn-primary">${students[i].password}</a>
+//   </div>
+// </li>`
+   
+    
+// }
+
+
+
+// constructor function
+
+
+// function Student (n,e,p,i){
+//    this.name =n ;
+//    this.email = e;
+//    this.password =p;
+//    this.image = i;
+//    this.info = function(){
+//       console.log(this.password);
+      
+//    }
+// }
+// var st1 = new Student("hira", 'hira@gmail.com',1234,'two.png')
+// console.log(st1);
+// st1.info()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
