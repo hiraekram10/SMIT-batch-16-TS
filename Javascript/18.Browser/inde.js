2119a039fc74c791d29c077b2e3dce0785323674
Umra-Badar20 <umrabadar555@gmail.com>
2025-10-02 20:06:38 +0500

    started browser and 2nd quiz done

---
+++ Javascript/18.Browser/inde.js
@@ -0,0 +1,16 @@
+// function move (){
+
+
+
+//     location.href = 'hello.html'
+// }
+// console.log(location.pathname);
+// window.location.hash
+// '#hello'
+// window.location.href = "http://www.youtube.com"
+// window.location.assign("http://www.me.com");
+
+
+// window.location.assign("hello.html"); no back option
+
+window.location.replace("hello.html");  
